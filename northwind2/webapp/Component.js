1807ba2697a4bc190855f92ebcdb9c7fc649a8ba
Pawan Khantwal <pawankhantwal16@gmail.com>
2021-12-27 17:15:24 +0000

    Master-detail app v1.0

--- northwind2/webapp/Component.js
+++ northwind2/webapp/Component.js
@@ -26,6 +26,31 @@ sap.ui.define([
                 // set the device model
                 this.setModel(models.createDeviceModel(), "device");
             }
+            // createContent: function(){
+            //     var oAppPage = new sap.ui.view({
+            //         viewName: "northwind.northwind2.view.App",
+            //         id:"idApp",
+            //         type:"XML"
+            //     });
+            //     var oProductPage = new sap.ui.view({
+            //         viewName:"northwind.northwind2.view.Products",
+            //         id:"idProduct",
+            //         type:"XML"
+            //     });
+            //     var oOrderPage = new sap.ui.view({
+            //         viewName="northwind.northwind2.view.Orders",
+            //         id:"idOrder",
+            //         type:"XML"
+            //     });
+            //     var oEmptyPage = new sap.ui.view({
+            //         viewName = "northwind.northwind2.view.Empty",
+            //         id:"idEmpty",
+            //         type:"XML"
+            //     });
+            //     var oAppCon = oAppPage.byId("idAppCon");
+            //     oAppCon.addMasterPage().addDetailPage(oEmptyPage).addDetailPage();
+            //     return oAppPage;
+            // }
         });
     }
 );
---
+++ northwind2/webapp/controller/Empty.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+    'sap/ui/core/mvc/Controller'
+],function(Controller){
+    return Controller.extend("northwind.northwind2.controller.Empty",{
+            
+    });
+});
